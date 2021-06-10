a99c9374dbd825b136f59c1c05fd1f632da30b4c
jeet adhikari <jeet@backbase.com>
2021-06-10 20:43:15 +0200

    testing CI/CD with small console log change

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
+  console.log('Starting...')
   if (!process.env.JWT_KEY) {
     throw new Error("No Jwt secret");
   }
--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -2,6 +2,7 @@ import { OrderCreatedListener } from "./events/listeners/order-created-listener"
 import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
+  console.log('Starting...')
   if (!process.env.NATS_CLUSTER_ID) {
     throw new Error("No NATS cluster id");
   }
