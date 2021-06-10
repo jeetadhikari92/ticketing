3028ef8e0efe57396778a50837cb6332996a2fd4
jeet adhikari <jeet@backbase.com>
2021-06-10 16:23:56 +0200

    fix for test

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -80,7 +80,7 @@ it('returns a 201 with valid inputs', async () => {
     .expect(201);
 
   const stripeCharges = await stripe.charges.list({ limit: 50 });
-  const stripeCharge = stripeCharges.data.find((charge) => {
+  const stripeCharge = stripeCharges.data.find((charge: {amount: number}) => {
     return charge.amount === price * 100;
   });
 
