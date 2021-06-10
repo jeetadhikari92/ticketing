8057606ee8910cfae496a43e86ea1ff2fae82473
jeet adhikari <jeet@backbase.com>
2021-06-10 22:15:45 +0200

    updated client with domain name

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ export default ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.ticketing-jeet-prod.xyz',
       headers: req.headers
     });
   } else {
